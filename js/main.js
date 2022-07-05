b4308cc544914a5e0c16fc54f7f6e99d5176e5d0
Elsayed Abdullah <sayedaladway@gmail.com>
2022-07-06 00:29:43 +0200

    created coinsurf homepage and about-us page

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+  // switch between app tutorials
+  $(".app-hero .platforms ul li.app").on("click", function () {
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+    $(".apps-download > div").hide();
+    $($(this).data("content")).fadeIn();
+  });
+});
+
+
+$('button.menu-toggle').on('click', function() {
+  $(this).toggleClass('is-active');
+  $('body').toggleClass('overlay')
+});
